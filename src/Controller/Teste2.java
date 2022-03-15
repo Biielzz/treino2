a36763961fe8f263d130b9b3557abe072ca93bec
ge621 <ge621@DESKTOP-OKPEKRO>
2022-03-15 20:16:25 -0300

    treino2

---
+++ src/Controller/Teste2.java
@@ -0,0 +1,14 @@
+package Controller;
+
+public class Teste2 {
+
+	public static void main(String[] args) {
+		String Semaforo[] = { "Vermelho", "Amarelo", "Verde" };
+		System.out.printf("A Ordem do Semáforo é:\n");
+		for(String sinal: Semaforo) {
+			System.out.printf("\n" + sinal);
+		}
+
+	}
+
+}
